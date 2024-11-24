51a7393fb460c7d674dcdd313f77c2595b7d2354
Abdul-Quddus <abdulquddusinda@gmail.com>
2024-11-24 14:49:45 +0100

    made it template

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Render!"
+    return render_template('index.html', app_name="My Cool Flask App", message="This is rendered dynamically!")
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
